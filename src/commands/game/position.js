70d4f7f845f40d852bff479bac7e51c1ded203bb
JPBBerry <jb@jt3ch.net>
2019-10-14 17:23:42 -0400

    Refactor position command

--- src/commands/game/position.js
+++ src/commands/game/position.js
@@ -1,24 +1,25 @@
-module.exports.load = client => {
-  const command = {
-    name: 'Pos',
-    category: 'game',
-    description: 'Quick view the user position.',
-    usage: `${client.settings.bot.prefix}pos`,
-    requiredPermission: null,
-    hasAccountCheck: true,
+const Command = require('../command.js')
 
-    async run (message) {
-      const entry = await client.database.collection('users').findOne({ uid: message.author.id })
-      message.channel.send(
-        new client.discord.MessageEmbed()
-          .setColor(client.settings.bot.embedColor)
-          .setTitle(':map: Position')
-          .setDescription(`x:\`${entry.xPos}\` y:\`${entry.yPos}\``)
-          .setFooter(message.author.tag)
-          .setTimestamp()
-      )
-    }
+module.exports = class PositionCommand extends Command {
+  constructor (client) {
+    super('position', ['pos'], 'Quick view the user position.', {
+      usage: `${client.settings.bot.prefix}position`,
+      accountCheck: true,
+      requiredPermission: null,
+      category: 'game'
+    })
+    this.c = client
   }
 
-  client.commands.push(command)
+  async run (message, args) {
+    const entry = await this.c.database.collection('users').findOne({ uid: message.author.id })
+    message.channel.send(
+      new this.c.discord.MessageEmbed()
+        .setColor(this.c.settings.bot.embedColor)
+        .setTitle(':map: Position')
+        .setDescription(`X:\`${entry.xPos}\` Y:\`${entry.yPos}\``)
+        .setFooter(message.author.tag)
+        .setTimestamp()
+    )
+  }
 }
