fc6bcefdea7dc968da56be177837dbad293edb4d
JPBBerry <jb@jt3ch.net>
2019-10-14 17:23:58 -0400

    Refactor set command

--- src/commands/game/set.js
+++ src/commands/game/set.js
@@ -1,37 +1,28 @@
-module.exports.load = client => {
-  const command = {
-    name: 'Set',
-    category: 'game',
-    description: 'Setttings',
-    usage: `${client.settings.bot.prefix}set {setting}`,
-    requiredPermission: null,
-    hasAccountCheck: true,
+const Command = require('../command.js')
 
-    async run (message) {
-      const setting = message.content.split(' ').splice(1)[0].toLowerCase()
-      if (setting === 'city') {
-        const { 1: currentName, 2: newName } = message.content.split(' ').splice(1)
-        if (!currentName || !newName) return client.sendError(message, `Missing argument, do ${client.settings.bot.prefix}set city {old-name} {new-name}`)
-        const currentCity = await client.database.collection('map').findOne({ 'city.name': currentName, 'city.owner': message.author.id })
-        if (!currentCity) return client.sendError(message, 'City was not found / you don\'t own it!')
-        client.game.setCityName(message.author.id, currentCity.xPos, currentCity.yPos, newName)
-          .then(() => {
-            client.sendSuccess(message, `Set city name from \`${currentCity.city.name}\` to \`${newName}\``)
-          })
-          .catch(e => client.sendError(message, e))
-      } else if (setting === 'empire') {
-        const newName = message.content.split(' ').splice(1)[1]
-        if (!newName) return client.sendError(message, `Missing argument, do ${client.settings.bot.prefix}set empire {empire-name}`)
-        client.game.setEmpireName(message.author.id, newName)
-          .then(() => {
-            client.sendSuccess(message, `Set your empire name to \`${newName}\`!`)
-          })
-          .catch(e => client.sendError(message, e))
-      } else {
-        client.sendError(message, `Invalid setting \`${setting}\`\n\nChoose from: \`city\`, \`empire\``)
-      }
-    }
+module.exports = class SetCommand extends Command {
+  constructor (client) {
+    super('settings', ['set'], 'Setttings', {
+      usage: `${client.settings.bot.prefix}settings {setting}`,
+      accountCheck: true,
+      requiredPermission: null,
+      category: 'game'
+    })
+    this.c = client
   }
 
-  client.commands.push(command)
+  async run (message, args) {
+    const setting = args[0].toLowerCase()
+    if (setting === 'empire') {
+      const newName = args[1]
+      if (!newName) return this.c.sendError(message, `Missing argument, do ${this.c.settings.bot.prefix}set empire {empire-name}`)
+      this.c.game.setEmpireName(message.author.id, newName)
+        .then(() => {
+          this.c.sendSuccess(message, `Set your empire name to \`${newName}\`!`)
+        })
+        .catch(e => this.c.sendError(message, e))
+    } else {
+      this.c.sendError(message, `Invalid setting \`${setting}\`\n\nChoose from: \`empire\``)
+    }
+  }
 }
