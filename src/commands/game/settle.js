6c4692f5a3706b5427d0288240f91a7d0f280f52
JPBBerry <jb@jt3ch.net>
2019-10-14 17:24:22 -0400

    Refactor settle command

--- src/commands/game/settle.js
+++ src/commands/game/settle.js
@@ -1,57 +1,58 @@
-module.exports.load = client => {
-  const command = {
-    name: 'Settle',
-    category: 'game',
-    description: 'Settle the tile you\'re currently on.',
-    usage: `${client.settings.bot.prefix}settle {name}`,
-    requiredPermission: null,
-    hasAccountCheck: true,
+const Command = require('../command.js')
 
-    async run (message) {
-      const name = message.content.split(' ').splice(1)[0]
-      if (!name) return client.sendError(message, `Name is required, \`${client.settings.bot.prefix}settle {name}\``)
-      const user = await client.database.collection('users').findOne({ uid: message.author.id })
-      message.channel.send(
-        new client.discord.MessageEmbed()
-          .setColor(client.settings.bot.embedColor)
-          .setTitle('Confirm settle')
-          .setDescription(`Settle Tile: X: \`${user.xPos}\`, Y: \`${user.yPos}\``)
-          .addField('Name', name)
-          .setFooter(message.author.tag)
-          .setTimestamp()
-      )
-        .then(confirmMsg => {
-          client.confirm(message, confirmMsg, {
-            no: () => {
-              confirmMsg.edit(
-                new client.discord.MessageEmbed()
-                  .setColor(client.settings.bot.embedColor)
-                  .setTitle('Cancelled settle')
-                  .setFooter(message.author.tag)
-                  .setTimestamp()
-              )
-            },
-            notime: () => {
-              client.sendError(message, 'Did not react in time, cancelled', confirmMsg)
-            },
-            yes: () => {
-              client.game.settleLocation(message.author.id, name)
-                .then(() => {
-                  confirmMsg.edit(
-                    new client.discord.MessageEmbed()
-                      .setColor(client.settings.bot.embedColor)
-                      .setTitle(`Settled tile! Welcome ${name} to the world!`)
-                      .setDescription(`You've successfully claimed tile: X: \`${user.xPos}\`, Y: \`${user.yPos}\``)
-                      .setFooter(message.author.tag)
-                      .setTimestamp()
-                  )
-                })
-                .catch(e => client.sendError(message, e, confirmMsg))
-            }
-          })
-        })
-    }
+module.exports = class SettleCommand extends Command {
+  constructor (client) {
+    super('settle', [], 'Settle the tile you\'re currently on.', {
+      usage: `${client.settings.bot.prefix}settle`,
+      accountCheck: true,
+      requiredPermission: null,
+      category: 'game'
+    })
+    this.c = client
   }
 
-  client.commands.push(command)
+  async run (message, args) {
+    const name = args[0]
+    if (!name) return this.c.sendError(message, `Name is required, \`${this.c.settings.bot.prefix}settle {name}\``)
+    const user = await this.c.database.collection('users').findOne({ uid: message.author.id })
+    message.channel.send(
+      new this.c.discord.MessageEmbed()
+        .setColor(this.c.settings.bot.embedColor)
+        .setTitle('Confirm settle')
+        .setDescription(`Settle Tile: X: \`${user.xPos}\`, Y: \`${user.yPos}\``)
+        .addField('Name', name)
+        .setFooter(message.author.tag)
+        .setTimestamp()
+    )
+      .then(confirmMsg => {
+        this.c.confirm(message, confirmMsg, {
+          no: () => {
+            confirmMsg.edit(
+              new this.c.discord.MessageEmbed()
+                .setColor(this.c.settings.bot.embedColor)
+                .setTitle('Cancelled settle')
+                .setFooter(message.author.tag)
+                .setTimestamp()
+            )
+          },
+          notime: () => {
+            this.c.sendError(message, 'Did not react in time, cancelled', confirmMsg)
+          },
+          yes: () => {
+            this.c.game.settleLocation(message.author.id, name)
+              .then(() => {
+                confirmMsg.edit(
+                  new this.c.discord.MessageEmbed()
+                    .setColor(this.c.settings.bot.embedColor)
+                    .setTitle(`Settled tile! Welcome ${name} to the world!`)
+                    .setDescription(`You've successfully claimed tile: X: \`${user.xPos}\`, Y: \`${user.yPos}\``)
+                    .setFooter(message.author.tag)
+                    .setTimestamp()
+                )
+              })
+              .catch(e => this.c.sendError(message, e, confirmMsg))
+          }
+        })
+      })
+  }
 }
