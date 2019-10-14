9981beeca97a93a948e67264ab8b4de4ed120700
JPBBerry <jb@jt3ch.net>
2019-10-14 17:24:35 -0400

    Refactor help command

--- src/commands/general/help.js
+++ src/commands/general/help.js
@@ -1,50 +1,51 @@
-module.exports.load = client => {
-  const command = {
-    name: 'Help',
-    category: 'general',
-    description: 'Show\'s all commands and how to use them.',
-    usage: `${client.settings.bot.prefix}help`,
-    requiredPermission: null,
-    hasAccountCheck: false,
+const Command = require('../command.js')
 
-    run (message) {
-      const cmd = message.content.split(' ').splice(1)[0]
-      if (cmd) {
-        const command = client.commands.find(x => x.name.toLowerCase() === cmd.toLowerCase())
-        if (!command || command.category === 'dev') return client.sendError(message, `Invalid command: \`${cmd}\``)
-        return message.channel.send(
-          new client.discord.MessageEmbed()
-            .setColor(client.settings.bot.embedColor)
-            .setTitle(command.name)
-            .addField('Description', `\`\`\`${command.description}\`\`\``)
-            .addField('Usage', `\`\`\`${command.usage}\`\`\``)
-            .setFooter(message.author.tag)
-            .setTimestamp()
-        )
-      }
-      const embed = new client.discord.MessageEmbed()
-        .setColor(client.settings.bot.embedColor)
-        .setTitle('Help')
-        .setDescription(`Use \`${client.settings.bot.prefix}help {command}\` to learn more about the command.`)
-        .setFooter(message.author.tag)
-        .setTimestamp()
-
-      client.commands.map(x => x.category).filter(x => x !== 'dev') // remove dev
-        .reduce((a, b) => { // remove dupes
-          a = a || []
-          if (!a.includes(b)) a.push(b)
-          return a
-        }, [])
-        .forEach(category => {
-          embed.addField(category.charAt(0).toUpperCase() + category.slice(1), // make 1st character uppercase
-            '```' + '\n' +
-                    client.commands.filter(x => x.category === category).map(x => x.name.toLowerCase()).join('\n') + '\n' +
-                    '```'
-          )
-        })
-      message.channel.send(embed)
-    }
+module.exports = class HelpCommand extends Command {
+  constructor (client) {
+    super('help', ['h'], 'Show\'s all commands and how to use them.', {
+      usage: `${client.settings.bot.prefix}help`,
+      accountCheck: false,
+      requiredPermission: null,
+      category: 'general'
+    })
+    this.c = client
   }
 
-  client.commands.push(command)
+  async run (message, args) {
+    const cmd = args[0]
+    if (cmd) {
+      const command = this.c.commands.find(x => x.name.toLowerCase() === cmd.toLowerCase() || x.aliases.includes(cmd.toLowerCase))
+      if (!command || command.category === 'dev') return this.c.sendError(message, `Invalid command: \`${cmd}\``)
+      return message.channel.send(
+        new this.c.discord.MessageEmbed()
+          .setColor(this.c.settings.bot.embedColor)
+          .setTitle(command.name)
+          .addField('Description', `\`\`\`${command.description}\`\`\``)
+          .addField('Usage', `\`\`\`${command.usage}\`\`\``)
+          .setFooter(message.author.tag)
+          .setTimestamp()
+      )
+    }
+    const embed = new this.c.discord.MessageEmbed()
+      .setColor(this.c.settings.bot.embedColor)
+      .setTitle('Help')
+      .setDescription(`Use \`${this.c.settings.bot.prefix}help {command}\` to learn more about the command.`)
+      .setFooter(message.author.tag)
+      .setTimestamp()
+
+    this.c.commands.map(x => x.category).filter(x => x !== 'dev') // remove dev
+      .reduce((a, b) => { // remove dupes
+        a = a || []
+        if (!a.includes(b)) a.push(b)
+        return a
+      }, [])
+      .forEach(category => {
+        embed.addField(category.charAt(0).toUpperCase() + category.slice(1), // make 1st character uppercase
+          '```' + '\n' +
+                    this.c.commands.filter(x => x.category === category).map(x => x.name.toLowerCase()).join('\n') + '\n' +
+                    '```'
+        )
+      })
+    message.channel.send(embed)
+  }
 }
