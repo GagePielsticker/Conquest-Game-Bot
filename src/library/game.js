bfa9a0578edaa5821459625d37a16db0b825b525
JPBBerry <jb@jt3ch.net>
2019-10-14 13:00:20 -0400

    added lib function for leaderboard

--- src/library/game.js
+++ src/library/game.js
@@ -668,6 +668,43 @@ module.exports = client => {
       return Promise.resolve({ cities: outputArray, totalPages: userEntry.cities.length })
     },
 
+    getLeaderboard: async (by, pageNumber) => {
+      let tempList = []
+      let sortedList
+
+      if (by == 'city') {
+        const cities = (await client.database.collection('map').find({'city.level': {$exists: true}}).toArray()).filter(x=>x.city.owner)
+        cities.forEach(x => {
+          if(!tempList[x.city.owner]) tempList[x.city.owner] = 0
+          tempList[x.city.owner]++
+        })
+      } else if (by == 'gold') {
+        const users = await client.database.collection('users').find({}).toArray()
+        users.forEach(user => {
+          tempList[user.uid] = user.gold
+        })
+      } else if (by === 'population') {
+        const users = await client.database.collection('users').find({}).toArray()
+        users.forEach(user => {
+          tempList[user.uid] = user.cities.reduce((a,b) => a + Object.values(b.population).reduce((c, d) => c+d, 0), 0)
+        })
+      }
+      sortedList = Object.keys(tempList).sort((a,b) => {
+        if(tempList[a] > tempList[b]) return -1
+        else if(tempList[a] < tempList[b]) return 1
+        else return 0
+      })
+      let i = 0
+      return sortedList.map(x => {
+        i++
+        return {
+          index: i,
+          user: x
+        }
+      })
+      .slice((pageNumber - 1) * 5, pageNumber * 5)
+    },
+
     /**
      * Gets the city by the name
      * @param {String} uid
