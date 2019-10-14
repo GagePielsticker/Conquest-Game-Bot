563c482bf2a3ad67194732add258b4d33f57f24a
JPBBerry <jb@jt3ch.net>
2019-10-14 17:23:34 -0400

    Refactor move command

--- src/commands/game/move.js
+++ src/commands/game/move.js
@@ -1,85 +1,85 @@
-module.exports.load = client => {
-  const humanizeDuration = require('humanize-duration')
-  const moment = require('moment')
+const Command = require('../command.js')
+const humanizeDuration = require('humanize-duration')
+const moment = require('moment')
 
-  const command = {
-    name: 'Move',
-    category: 'game',
-    description: 'Move to a location',
-    usage: `${client.settings.bot.prefix}move {x} {y}`,
-    requiredPermission: null,
-    hasAccountCheck: true,
+module.exports = class MoveCommand extends Command {
+  constructor (client) {
+    super('move', ['m'], 'Move to a location', {
+      usage: `${client.settings.bot.prefix}move {x} {y}`,
+      accountCheck: true,
+      requiredPermission: null,
+      category: 'game'
+    })
+    this.c = client
+  }
 
-    async run (message) {
-      const cooldown = client.game.movementCooldown.has(message.author.id)
-      if (cooldown) return client.sendError(message, `You're already moving, wait ${humanizeDuration(cooldown.endTime - moment().unix())}`)
+  async run (message, args) {
+    const cooldown = this.c.game.movementCooldown.has(message.author.id)
+    if (cooldown) return this.c.sendError(message, `You're already moving, wait ${humanizeDuration(cooldown.endTime - moment().unix())}`)
 
-      let { 0: newX, 1: newY } = message.content.split(' ').splice(1)
-      if (!newX || !newY) return client.sendError(message, `Missing X or Y, do ${client.settings.bot.prefix}move {x} {y}`)
+    let { 0: newX, 1: newY } = args
+    if (!newX || !newY) return this.c.sendError(message, `Missing X or Y, do ${this.c.settings.bot.prefix}move {x} {y}`)
 
-      if (isNaN(newX)) return client.sendError(message, 'Invalid X')
-      if (isNaN(newY)) return client.sendError(message, 'Invalid Y')
+    if (isNaN(newX)) return this.c.sendError(message, 'Invalid X')
+    if (isNaN(newY)) return this.c.sendError(message, 'Invalid Y')
 
-      newX = Number(newX)
-      newY = Number(newY)
+    newX = Number(newX)
+    newY = Number(newY)
 
-      const user = await client.database.collection('users').findOne({ uid: message.author.id })
-      const timeToMove = await client.game.calculateTravelTime(user.xPos, user.yPos, newX, newY)
+    const user = await this.c.database.collection('users').findOne({ uid: message.author.id })
+    const timeToMove = await this.c.game.calculateTravelTime(user.xPos, user.yPos, newX, newY)
 
-      message.channel.send(
-        new client.discord.MessageEmbed()
-          .setColor(client.settings.bot.embedColor)
-          .setTitle('Confirm movement')
-          .addField('From tile', `X: \`${user.xPos}\`, Y: \`${user.yPos}\``, true)
-          .addField('To tile', `X: \`${newX}\`, Y: \`${newY}\``)
-          .addField('Will Take', `\`${humanizeDuration(timeToMove)}\``)
-          .setFooter(message.author.tag)
-          .setTimestamp()
-      )
-        .then(msg => {
-          client.confirm(message, msg, {
-            no: () => {
-              msg.edit(
-                new client.discord.MessageEmbed()
-                  .setColor(client.settings.bot.embedColor)
-                  .setTitle('Cancelled Movement')
-                  .setFooter(message.author.tag)
-                  .setTimestamp()
-              )
-            },
-            notime: () => {
-              client.sendError(message, 'Ran out of time, cancelled', msg)
-            },
-            yes: () => {
-              client.game.moveUser(message.author.id, newX, newY)
-                .then((time) => {
-                  if (message.content.match(/-d/) && client.beta) time = 1000
-                  msg.edit(
-                    new client.discord.MessageEmbed()
-                      .setColor(client.settings.bot.embedColor)
-                      .setTitle('Moving!')
-                      .setDescription(`Now moving to tile: X: \`${newX}\`, Y: \`${newY}\`\n\nYou will be pinged here when you have finished moving!`)
-                      .addField('Will be done in', `\`${humanizeDuration(time)}\``)
+    message.channel.send(
+      new this.c.discord.MessageEmbed()
+        .setColor(this.c.settings.bot.embedColor)
+        .setTitle('Confirm movement')
+        .addField('From tile', `X: \`${user.xPos}\`, Y: \`${user.yPos}\``, true)
+        .addField('To tile', `X: \`${newX}\`, Y: \`${newY}\``)
+        .addField('Will Take', `\`${humanizeDuration(timeToMove)}\``)
+        .setFooter(message.author.tag)
+        .setTimestamp()
+    )
+      .then(msg => {
+        this.c.confirm(message, msg, {
+          no: () => {
+            msg.edit(
+              new this.c.discord.MessageEmbed()
+                .setColor(this.c.settings.bot.embedColor)
+                .setTitle('Cancelled Movement')
+                .setFooter(message.author.tag)
+                .setTimestamp()
+            )
+          },
+          notime: () => {
+            this.c.sendError(message, 'Ran out of time, cancelled', msg)
+          },
+          yes: () => {
+            this.c.game.moveUser(message.author.id, newX, newY)
+              .then((time) => {
+                if (message.content.match(/-d/) && this.c.beta) time = 1000
+                msg.edit(
+                  new this.c.discord.MessageEmbed()
+                    .setColor(this.c.settings.bot.embedColor)
+                    .setTitle('Moving!')
+                    .setDescription(`Now moving to tile: X: \`${newX}\`, Y: \`${newY}\`\n\nYou will be pinged here when you have finished moving!`)
+                    .addField('Will be done in', `\`${humanizeDuration(time)}\``)
+                    .setFooter(message.author.tag)
+                    .setTimestamp()
+                )
+                setTimeout(() => {
+                  message.reply(
+                    new this.c.discord.MessageEmbed()
+                      .setColor(this.c.settings.bot.embedColor)
+                      .setTitle('Moved!')
+                      .setDescription(`Your new location is X: \`${newX}\` \`${newY}\``)
                       .setFooter(message.author.tag)
                       .setTimestamp()
                   )
-                  setTimeout(() => {
-                    message.reply(
-                      new client.discord.MessageEmbed()
-                        .setColor(client.settings.bot.embedColor)
-                        .setTitle('Moved!')
-                        .setDescription(`Your new location is X: \`${newX}\` \`${newY}\``)
-                        .setFooter(message.author.tag)
-                        .setTimestamp()
-                    )
-                  }, time)
-                })
-                .catch(e => client.sendError(message, e))
-            }
-          })
+                }, time)
+              })
+              .catch(e => this.c.sendError(message, e))
+          }
         })
-    }
+      })
   }
-
-  client.commands.push(command)
 }
