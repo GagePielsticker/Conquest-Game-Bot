e148557399bf772854d292483d35196164fd2811
JPBBerry <jb@jt3ch.net>
2019-10-14 17:23:18 -0400

    Refactor citylist command

--- src/commands/game/citylist.js
+++ src/commands/game/citylist.js
@@ -1,38 +1,40 @@
-module.exports.load = client => {
-  const command = {
-    name: 'Citylist',
-    category: 'game',
-    description: 'City information',
-    usage: `${client.settings.bot.prefix}city`,
-    requiredPermission: null,
-    hasAccountCheck: true,
+const Command = require('../command.js')
 
-    async run (message) {
-      let page = message.content.split(' ').splice(1)[0] || 1
-      if (isNaN(page)) return client.sendError(message, 'Invalid number for page')
-      page = Number(page)
-      client.game.getUserCityNames(message.author.id, page)
-        .then(response => {
-          const { cities, totalPages } = response
-          if (page > totalPages) return client.sendError(message, `Invalid page, max pages: ${totalPages}`)
-          message.channel.send(
-            new client.discord.MessageEmbed()
-              .setColor(client.settings.bot.embedColor)
-              .setTitle('Your cities')
-              .setDescription(`You are viewing page \`${page}\` / \`${totalPages}\`` + '\n' +
-                    '```' + '\n' +
-                      `${cities.length < 1 ? 'none'
-                      : cities.map(x => `${x.index}.) ${x.name}`).join('\n')
-                    }` + '\n' +
-                    '```' + '\n\n' +
-                    `${page + 1 > totalPages ? '' : `Do \`${client.settings.bot.prefix}city list ${page + 1}\` to view next page`}`
-              )
-              .setFooter(message.author.tag)
-              .setTimestamp()
-          )
-        })
-        .catch(e => client.sendError(message, e))
-    }
+module.exports = class CityListCommand extends Command {
+  constructor (client) {
+    super('citylist', ['cl'], 'List your cities', {
+      usage: `${client.settings.bot.prefix}citylist`,
+      accountCheck: true,
+      requiredPermission: null,
+      category: 'game'
+    })
+    this.c = client
   }
-  client.commands.push(command)
+
+  async run (message, args) {
+    let page = args[0] || 1
+    if (isNaN(page)) return this.c.sendError(message, 'Invalid number for page')
+    page = Number(page)
+    this.c.game.getUserCityNames(message.author.id, page)
+      .then(response => {
+        const { cities, totalPages } = response
+        if (page > totalPages) return this.c.sendError(message, `Invalid page, max pages: ${totalPages}`)
+        message.channel.send(
+          new this.c.discord.MessageEmbed()
+            .setColor(this.c.settings.bot.embedColor)
+            .setTitle('Your cities')
+            .setDescription(`You are viewing page \`${page}\` / \`${totalPages}\`` + '\n' +
+                  '```' + '\n' +
+                    `${cities.length < 1 ? 'none'
+                    : cities.map(x => `${x.index}.) ${x.name}`).join('\n')
+                  }` + '\n' +
+                  '```' + '\n\n' +
+                  `${page + 1 > totalPages ? '' : `Do \`${this.c.settings.bot.prefix}city list ${page + 1}\` to view next page`}`
+            )
+            .setFooter(message.author.tag)
+            .setTimestamp()
+        )
+      })
+      .catch(e => this.c.sendError(message, e))
+  }
 }
