6c4aad683b6d7179defa1edb64bd53de58dab28d
JPBBerry <jb@jt3ch.net>
2019-10-14 17:24:49 -0400

    Refactor stats command

--- src/commands/general/stats.js
+++ src/commands/general/stats.js
@@ -1,40 +1,41 @@
-module.exports.load = client => {
-  const si = require('systeminformation')
-  const humanizeDuration = require('humanize-duration')
+const Command = require('../command.js')
+const si = require('systeminformation')
+const humanizeDuration = require('humanize-duration')
 
-  const command = {
-    name: 'Stats',
-    category: 'general',
-    description: 'See bot statistics.',
-    usage: `${client.settings.bot.prefix}stats`,
-    requiredPermission: null,
-    hasAccountCheck: false,
+module.exports = class StatsCommand extends Command {
+  constructor (client) {
+    super('stats', ['s', 'stat'], 'Get\'s stats for the bot', {
+      usage: `${client.settings.bot.prefix}invite`,
+      accountCheck: false,
+      requiredPermission: null,
+      category: 'general'
+    })
+    this.c = client
+  }
 
-    async run (message) {
-      const cpu = await si.cpu()
-      const mem = await si.mem()
-      const operating = await si.osInfo()
-      const embed = new client.discord.MessageEmbed()
+  async run (message, args) {
+    const cpu = await si.cpu()
+    const mem = await si.mem()
+    const operating = await si.osInfo()
+    message.channel.send(
+      new this.c.discord.MessageEmbed()
         .setTitle(':bar_chart: Statistics')
-        .setDescription(client.settings.bot.botDescription)
-        .addField('Users', `\`${client.users.size}\``, true)
-        .addField('Guilds', `\`${client.guilds.size}\``, true)
+        .setDescription(this.c.settings.bot.botDescription)
+        .addField('Users', `\`${this.c.users.size}\``, true)
+        .addField('Guilds', `\`${this.c.guilds.size}\``, true)
         .addField('Language', '`NodeJS`', true)
         .addField('RAM\'s', `\`${Math.floor(mem.used / 1000000000)}gb/${Math.floor(mem.total / 1000000000)}gb\``, true)
         .addField('CPU', `\`${cpu.cores} Cores\``, true)
         .addField('Platform', `\`${operating.platform}\``, true)
-        .addField('Shards', `\`${parseInt(client.options.shards) + 1}\``, true)
-        .addField('Ping', `\`${client.ws.ping} ms\``, true)
-        .addField('Uptime', `\`${humanizeDuration(client.uptime)}\``, true)
-        .addField('Invite Link', `[Click Here](${client.settings.bot.inviteURL})`, true)
-        .addField('Support Server', `[Click Here](${client.settings.bot.supportServer})`, true)
+        .addField('Shards', `\`${parseInt(this.c.options.shards) + 1}\``, true)
+        .addField('Ping', `\`${this.c.ws.ping} ms\``, true)
+        .addField('Uptime', `\`${humanizeDuration(this.c.uptime)}\``, true)
+        .addField('Invite Link', `[Click Here](${this.c.settings.bot.inviteURL})`, true)
+        .addField('Support Server', `[Click Here](${this.c.settings.bot.supportServer})`, true)
         .addField('Developer', '`uber#0001\nJPBBerry#0001`', true)
         .setFooter(`${message.author.username}#${message.author.discriminator}`)
         .setTimestamp()
-        .setColor(client.settings.bot.embedColor)
-      message.channel.send(embed)
-    }
+        .setColor(this.c.settings.bot.embedColor)
+    )
   }
-
-  client.commands.push(command)
 }
